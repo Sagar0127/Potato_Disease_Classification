15d9be6c3b271a392d35c174a55644d222d05232
Sagar0127 <sagarv1127@gmail.com>
2024-11-21 15:57:02 +0530

    commit

---
+++ api/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
